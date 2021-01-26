c1852304deba253273f3e71cb747f385a19be5f3
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-01-26 13:14:21 -0600

    finds warehouse that has cheetos

--- joins-solution.sql
+++ joins-solution.sql
@@ -8,3 +8,10 @@ SELECT orders.id, line_items.quantity, products.description FROM orders
 JOIN line_items ON orders.id = line_items.order_id
 JOIN products ON products.id = line_items.product_id
 ORDER BY orders.id;
+
+--3. Which warehouses have cheetos?
+SELECT warehouse, products.description FROM warehouse
+JOIN warehouse_product ON warehouse.id = warehouse_product.warehouse_id
+JOIN products ON warehouse_product.product_id = products.id
+WHERE products.id = 5
+ORDER BY warehouse.warehouse;
